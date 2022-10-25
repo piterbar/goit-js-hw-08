fd37ff8be4cdde75a90b20c0567339cf25a4773e
Piotr Baraniak <piterb86@gmail.com>
2022-10-25 10:38:47 +0200

    test next

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -32,15 +32,16 @@ function onTextareaInput(e) {
 function populateTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (localStorage.length > 0) {
-    //formData.email = savedMessage.email;
+    formData.email = savedMessage.email;
     formData.message = savedMessage.message;
       
     if (savedMessage === null) {
     
       return;
     }
+  
+    refs.textarea.value = savedMessage['message'] || '';
+    refs.input.value = savedMessage['email'] || '';
   }
-  refs.textarea.value = savedMessage['message'] || '';
-  refs.input.value = savedMessage['email'] || '';
 }
-//test branch load 
+//test branch load 5
